822640e05d0a1723b07e16f3495f716718fd74ac
yanqirenshi <yanqirenshi@gmail.com>
2024-01-04 16:38:32 +0900

    var 0.0.4

--- src/components/P.js
+++ src/components/P.js
@@ -3,8 +3,11 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 
 export default function P (props) {
+    const children = props.children;
+
     return (
         <Box>
+          {children}
         </Box>
     );
 }
--- src/components/S.js
+++ src/components/S.js
@@ -3,8 +3,11 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 
 export default function S (props) {
+    const children = props.children;
+
     return (
         <Typography>
+          {children}
         </Typography>
     );
 }
